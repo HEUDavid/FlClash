feb1d366851ee34ba72a3d3a6536138bd640cbf1
HEUDavid <admin@mdavid.cn>
2026-08-29 03:24:18 +0800

    Revert "upt ui"
    This reverts commit 15366f53dad6ce3d9479ff6be0c2c538970cf1cb.

--- lib/custom/mvp_view.dart
+++ lib/custom/mvp_view.dart
@@ -772,7 +772,7 @@ class _MvpProfileCard extends StatelessWidget {
                               alignment: PlaceholderAlignment.middle,
                               child: Icon(
                                 Icons.delete_outline_rounded,
-                                size: 12,
+                                size: 14,
                                 color: MvpTheme.dangerText,
                               ),
                             ),
@@ -811,7 +811,7 @@ class _MvpProfileCard extends StatelessWidget {
                               alignment: PlaceholderAlignment.middle,
                               child: Icon(
                                 Icons.keyboard_arrow_up_rounded,
-                                size: 12,
+                                size: 14,
                                 color: MvpTheme.textSecondary,
                               ),
                             ),
