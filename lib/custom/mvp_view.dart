ac76b9918d054daa8c218b6cdf34a4e13bbf056a
HEUDavid <admin@mdavid.cn>
2026-09-05 20:43:53 +0800

    ui upt btn

--- lib/custom/mvp_view.dart
+++ lib/custom/mvp_view.dart
@@ -583,7 +583,7 @@ class _MvpStatusHero extends StatelessWidget {
             ),
           ),
         ),
-        const SizedBox(height: 6),
+        const SizedBox(height: 16),
         AnimatedSwitcher(
           duration: const Duration(milliseconds: 200),
           child: Text(
