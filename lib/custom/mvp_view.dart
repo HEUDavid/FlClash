e8e8f1cd11b8f1c9194fad13a8590b266b7c6c15
HEUDavid <admin@mdavid.cn>
2026-09-05 17:43:30 +0800

    ui upt for input border

--- lib/custom/mvp_view.dart
+++ lib/custom/mvp_view.dart
@@ -1043,7 +1043,7 @@ class _MvpProfileCard extends StatelessWidget {
                 color: MvpTheme.inputBg,
                 borderRadius: BorderRadius.circular(12),
                 border: Border.all(
-                  color: Colors.black.withValues(alpha: 0.10),
+                  color: Colors.black.withValues(alpha: 0.08),
                   width: 0.5,
                 ),
                 boxShadow: [
