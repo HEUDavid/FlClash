1c0addf1e6305a4e09e6145b5da82c6d15e908a2
HEUDavid <admin@mdavid.cn>
2026-09-06 17:55:48 +0800

    ui upt for line height

--- lib/custom/mvp_view.dart
+++ lib/custom/mvp_view.dart
@@ -1236,7 +1236,7 @@ class _MvpProfileCard extends StatelessWidget {
                           fontSize: 14,
                           fontWeight: FontWeight.w600,
                           color: Colors.white,
-                          height: 1.2,
+                          height: 1.14,
                           leadingDistribution: TextLeadingDistribution.even,
                         ),
                       ),
