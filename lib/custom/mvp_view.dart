5fdc773a36025d7826cf3a286a0ec65a0331a8b1
HEUDavid <admin@mdavid.cn>
2026-09-16 05:48:13 +0800

    upt

--- lib/custom/mvp_view.dart
+++ lib/custom/mvp_view.dart
@@ -885,7 +885,7 @@ class _MvpProfileCard extends StatelessWidget {
                       WidgetSpan(
                         alignment: PlaceholderAlignment.middle,
                         child: Icon(
-                          Icons.tune_outlined, // tune_rounded alt_route_rounded
+                          Icons.tune_outlined, // tune_rounded or alt_route_rounded
                           size: 16,
                           color: MvpTheme.textPrimary,
                         ),
