b40d0b7f3cbf3f49dbaec0f26c5a0e726fb3de6c
HEUDavid <admin@mdavid.cn>
2026-09-16 05:15:33 +0800

    upt

--- lib/custom/mvp_view.dart
+++ lib/custom/mvp_view.dart
@@ -1222,7 +1222,7 @@ class _MvpProfileCard extends StatelessWidget {
                                 ),
                               )
                             : const Icon(
-                                Icons.cloud_download_rounded,
+                                Icons.cloud_sync_rounded,
                                 size: 16,
                                 color: Colors.white,
                               ),
