bfb22883260e1136a23c3c71b70dd240c2738803
HEUDavid <admin@mdavid.cn>
2026-09-16 05:30:20 +0800

    upt

--- lib/custom/mvp_view.dart
+++ lib/custom/mvp_view.dart
@@ -455,7 +455,7 @@ class _MvpHeaderBar extends StatelessWidget {
                         ),
                       )
                     : Icon(
-                        Icons.bug_report_outlined,
+                        Icons.upload_file_outlined,
                         size: 16,
                         color: MvpTheme.textSecondary.withValues(alpha: 0.8),
                       ),
