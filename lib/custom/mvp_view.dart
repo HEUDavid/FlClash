a28681f990159c5f360ad9a6c6463996f38cc496
HEUDavid <admin@mdavid.cn>
2026-09-16 06:39:11 +0800

    upt

--- lib/custom/mvp_view.dart
+++ lib/custom/mvp_view.dart
@@ -971,7 +971,7 @@ class _MvpProfileCard extends StatelessWidget {
       behavior: HitTestBehavior.opaque,
       child: Container(
         height: 30,
-        padding: const EdgeInsets.symmetric(horizontal: 8),
+        padding: const EdgeInsets.only(left: 8, right: 10),
         alignment: Alignment.center,
         decoration: BoxDecoration(
           color: backgroundColor,
