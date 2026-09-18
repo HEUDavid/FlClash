61a47702203fb2218b986c48228e82640b5559d4
HEUDavid <admin@mdavid.cn>
2026-09-18 19:05:56 +0800

    upt

--- lib/custom/mvp_view.dart
+++ lib/custom/mvp_view.dart
@@ -910,7 +910,7 @@ class _MvpProfileCard extends StatelessWidget {
                     label: '重置',
                     color: MvpTheme.dangerText,
                     backgroundColor:
-                        MvpTheme.dangerColor.withValues(alpha: 0.06),
+                        MvpTheme.dangerColor.withValues(alpha: 0.05),
                     onTap: () => onToggleInputArea(true),
                   )
                 else if (hasProfile)
@@ -975,7 +975,7 @@ class _MvpProfileCard extends StatelessWidget {
         alignment: Alignment.center,
         decoration: BoxDecoration(
           color: backgroundColor,
-          borderRadius: BorderRadius.circular(9),
+          borderRadius: BorderRadius.circular(10),
         ),
         child: Text.rich(
           TextSpan(
